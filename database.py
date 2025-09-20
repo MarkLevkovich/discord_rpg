1da7e45d080dc6d5b4830114cae6020510859b41
MarkLevkovich <levmarkpost@gmail.com>
2025-09-20 15:36:51 +0300

    init_db()

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import traceback
 
 def connect(func):
     def wrapper(*args, **kwargs):
-        conn = sqlite3.connect(r'\game.db')
+        conn = sqlite3.connect(r'game.db')
         try:
             cur = conn.cursor()
             result = func(cur, *args, **kwargs)
@@ -17,3 +17,18 @@ def connect(func):
             conn.close()
         return result
     return wrapper
+
+@connect
+def init_db(cur) -> None:
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY,
+            player_id TEXT UNIQUE,
+            current_hp INTEGER,
+            max_hp INTEGER,
+            damage INTEGER
+    )""")
+
+
+if __name__=="__main__":
+    init_db()
